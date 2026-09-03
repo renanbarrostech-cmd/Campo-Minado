03b65e0da6afad00000ae6d339fb0abc3c3f41ef
Renan barros <renanbarros.tech@gmail.com>
2026-09-03 14:59:05 +0000

    feat: It darkens when you click the cell.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const ROWS = 9
-const COLS = 9
+const ROWS = 9;
+const COLS = 9;
 
-const elBoard = document.getElementById("board")
+const elBoard = document.getElementById("board");
 
-let board = []
+let board = [];
 
 function createBoard() {
   board = Array.from({ length: ROWS }, () =>
@@ -12,7 +12,7 @@ function createBoard() {
       revealed: false,
       flagged: false,
       neighbors: 0,
-    }))
+    })),
   );
 }
 
@@ -21,14 +21,32 @@ function render() {
 
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLS; c++) {
+      const cell = board[r][c];
       const div = document.createElement("div");
-      div.className = "cell";
+
+      let classes = "cell";
+      if (cell.revealed) classes += " revealed";
+      div.className = classes;
+
       div.dataset.row = r;
       div.dataset.col = c;
+
       elBoard.appendChild(div);
     }
   }
 }
+
+elBoard.addEventListener("click", (e) => {
+  const target = e.target.closest(".cell");
+  if (!target) return;
+
+  const r = Number(target.dataset.row);
+  const c = Number(target.dataset.col);
+
+  board[r][c].revealed = true;
+
+  render();
+});
 
 createBoard();
 render();
